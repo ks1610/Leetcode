3b8eaa649a10a7594f2c426b2f3f1e2f9a301f5f
Khanh (Spite) <118985514+ks1610@users.noreply.github.com>
2024-07-29 11:07:02 +0700

    Update Solution.cpp

--- Compare_Version_Numbers/Solution.cpp
+++ Compare_Version_Numbers/Solution.cpp
@@ -1,5 +1,8 @@
 #include<bits/stdc++.h>
 
+#define WHILE_VER1(ver1, q1, i)  while(i < ver1.size() && ver1[i] != '.'){ q1.push_back(ver1[i]); i++;}
+#define WHILE_VER2(ver2, q2, j)  while(j < ver2.size() && ver2[j] != '.'){ q2.push_back(ver2[j]); j++;}
+
 class Solution {
 public:
     //convert a string representation of a number into a long long integer
@@ -15,47 +18,44 @@ public:
     //make a compare
     int compareVersion(std::string ver1, std::string ver2) {
         int i = 0, j = 0;
+        std::string q1 = "", q2 = "";
+        //int i = 0, j = 0;
         //compare version 1 and version2
         while(i < ver1.size() && j < ver2.size()){
             std::string q1 = "", q2 = "";
-            while(i < ver1.size() && ver1[i] != '.'){
-                q1.push_back(ver1[i]);
-                i++;
-            }
-            while(j < ver2.size() && ver2[j] != '.'){
-                q2.push_back(ver2[j]);
-                j++;
-            }
+            
+            WHILE_VER1(ver1, q1, i)
+            WHILE_VER2(ver2, q2, j)
+            
             int p = solve(q1);
             int q = solve(q2);
+            
             if(p < q) return -1;
             else if(p > q) return 1;
             i++;
             j++;
-        }
+        }   
+      
         //checks if there are any remaining segments in version1
         while( i < ver1.size() )
         {
             std::string q1 = "" ;
-            while( i < ver1.size() && ver1[i]!='.')
-            {
-                q1.push_back( ver1[i] ) ;
-                i++ ;
-            }
+            
+            WHILE_VER1(ver1, q1, i)
+            
             int p = solve( q1 ) ; int s = 0 ;
             if( p > s )         return 1 ;
             else if( p < s )    return -1 ;
             i++ ;
         }
+
         //checks if there are any remaining segments in version2.
         while( j < ver2.size() ) 
         {
             std::string q2 = "" ;
-            while( j < ver2.size() && ver2[j]!='.')
-            {
-                q2.push_back( ver2[j] ) ;
-                j++ ;
-            }
+            
+            WHILE_VER2(ver2, q2, j)
+            
             int p = 0 ; int s = solve( q2 ) ;
             if( p > s )         return 1 ;
             else if( p < s )    return -1 ;
