5f76f2f7aa5311c6507a18efce03dee03befe3b1
Khanh Trinh (Spite) <118985514+ks1610@users.noreply.github.com>
2024-10-31 23:01:09 +0700

    Create Solution.cpp

---
+++ Restore_IP_Addresses/Solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+      std::vector<std::string> ans;
+      std::string k;
+        
+      for(int a = 1; a <= 3; a++){
+        for(int b = 1; b <= 3; b++){
+            for(int c = 1; c <= 3; c++){
+                for(int d = 1; d <= 3; d++){
+                    if(a+b+c+d == s.length()){
+                        int s1 = stoi(s.substr(0,a));
+                        int s2 = stoi(s.substr(a,b));
+                        int s3 = stoi(s.substr(a+b,c));
+                        int s4 = stoi(s.substr(a+b+c,d));
+                        k = std::to_string(s1)+"."+ std::to_string(s2)+"."+ std::to_string(s3)+"."+ std::to_string(s4);
+                        if(s1<=255 && s2<=255 && s3<=255 && s4<=255){
+                            if(k.length() == s.length()+3)
+                                ans.push_back(k);
+                        }
+                    }
+                }
+            }
+        }
+      }
+      return ans;
+    }
+};
